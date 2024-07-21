469c7d7589b90598dfcfe2f0492f718f7f6e65d6
walid gzouly <wagzouly@gmail.com>
2024-07-21 12:20:18 +0100

    fix issue

--- client3.py
+++ client3.py
@@ -28,6 +28,16 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
+try:
+    url = QUERY.format(random.random())
+    print(f"Connecting to: {url}")
+    response = urllib.request.urlopen(url)
+    quotes = json.loads(response.read())
+    print(quotes)
+except urllib.error.URLError as e:
+    print(f"Failed to connect: {e.reason}")
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
@@ -35,14 +45,16 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
-    return stock, bid_price, ask_price, price
+    price = (bid_price+ask_price)/2
+    return stock, ask_price, bid_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return 
+    return price_a/price_b
 
 
 # Main
@@ -54,6 +66,6 @@ if __name__ == "__main__":
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            print(f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}, price:%{price})")
 
-        print("Ratio %s" % getRatio(price, price))
+        print(f"Ratio{getRatio(price["ABC"], price["DEF"])}")
